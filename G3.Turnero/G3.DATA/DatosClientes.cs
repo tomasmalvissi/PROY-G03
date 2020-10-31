b8178be354a1e545222520bf1ba4caf2c57865bb
Tomás Malvissi <50890384+tomasmalvissi@users.noreply.github.com>
2020-10-31 02:11:36 -0300

    empece a modelar el proyecto data pero tengo mucho sueño

---
+++ G3.Turnero/G3.DATA/DatosClientes.cs
@@ -0,0 +1,89 @@
+﻿using G3.CLASS;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G3.DATA
+{
+    public class DatosClientes : DatosConexion  //no se si se podran cargar los datos, yo nunca use EF en WForm,
+                                                //por eso a las clases (cliente en este caso) le ponia los atrib privado
+                                                // y generaba un constructor public
+    {
+        public int ABMClientes(string accion, Cliente clientes) //no creo que necesitemos modificar un cliente por eso borre ese case
+        {
+            int resultado = 0;
+            string orden = string.Empty;
+
+            switch (accion)
+            {
+                case "Alta":
+                    orden = "insert into Clientes " + "values ("
+                                            + "'" + clientes.Nombre_Apellido + "'"
+                                            + ");";
+                    break;
+
+                
+                case "Eliminar":
+                    orden = "Delete from Clientes where Id = " + clientes.Id;
+                    break;
+
+
+            }
+
+            SqlCommand sqlcmd = new SqlCommand(orden, conexion);
+            try
+            {
+                AbrirConex();
+                resultado = sqlcmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Error al tratar de cargar los datos de Cliente", e);
+            }
+            finally
+            {
+                CerrarConex();
+                sqlcmd.Dispose();
+            }
+            return resultado;
+        }
+
+        public DataSet MostrarClientes(string cual) //tampoco creo que debamos mostrar los clientes pero lo dejo 
+                                                    //por las dudas, si te parece borralo, capaz tengamos q mostrarlo en
+                                                    //otra instancia pero podemos hacer un join con otros datos
+        {
+            string orden = string.Empty;
+            if (cual != "Todos")
+                orden = "select * from Clientes where Id = " + int.Parse(cual) + ";";
+            else
+                orden = "select * from Clientes;";
+
+            SqlCommand sqlcmd = new SqlCommand(orden, conexion);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter();
+
+            try
+            {
+                AbrirConex();
+                sqlcmd.ExecuteNonQuery();
+                da.SelectCommand = sqlcmd;
+                da.Fill(ds);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("error al listar clientes", e);
+            }
+            finally
+            {
+                CerrarConex();
+                sqlcmd.Dispose();
+            }
+            return ds;
+        }
+    }
+}
---
+++ G3.Turnero/G3.DATA/DatosTurnos.cs
@@ -0,0 +1,97 @@
+﻿using G3.CLASS;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G3.DATA
+{
+    public class DatosTurnos : DatosConexion
+    {
+        public int ABMTurnos(string accion, Turno turnos)
+        {
+            int resultado = 0;
+            string orden = string.Empty;
+
+            switch (accion)
+            {
+                case "Alta":
+                    orden = "insert into Turnos " + "values ("              //ver como se insertan las FK creo que lo estoy 
+                                                                            // haciendo mal pero son las 2:07 am un viernes
+                                            + "'" + turnos.DateTime + "',"
+                                            + "'" + turnos.TareaId + "',"
+                                            + "'" + turnos.ClienteId + "',"
+                                            + "'" + turnos.PeluqueroId + "'"
+                                            + ");";
+                    break;
+
+                case "Modificar":
+                    orden = "update Turnos set "
+                                        + "Nombre= '" + turnos.DateTime + "',"
+                                        + "Direccion= '" + turnos.TareaId + "',"
+                                        + "Telefono= '" + turnos.ClienteId + "',"
+                                        + "Email= '" + turnos.PeluqueroId + "'"
+                                        + "where Id= " + turnos.Id;
+                    break;
+
+                case "Eliminar":
+                    orden = "Delete from Turnos where Id = " + turnos.Id;
+                    break;
+
+
+            }
+
+            SqlCommand sqlcmd = new SqlCommand(orden, conexion);
+            try
+            {
+                AbrirConex();
+                resultado = sqlcmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Error al tratar de modificar los registros de Turnos", e);
+            }
+            finally
+            {
+                CerrarConex();
+                sqlcmd.Dispose();
+            }
+            return resultado;
+        }
+
+        public DataSet MostrarClientes(string cual)
+        {
+            string orden = string.Empty;
+            if (cual != "Todos")
+                orden = "select * from Turnos where Id = " + int.Parse(cual) + ";";
+            else
+                orden = "select * from Turnos;";
+
+            SqlCommand sqlcmd = new SqlCommand(orden, conexion);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter();
+
+            try
+            {
+                AbrirConex();
+                sqlcmd.ExecuteNonQuery();
+                da.SelectCommand = sqlcmd;
+                da.Fill(ds);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("error al listar Turnos", e);
+            }
+            finally
+            {
+                CerrarConex();
+                sqlcmd.Dispose();
+            }
+            return ds;
+        }
+    }
+}
